e988e8708f11fc50b41900bbcd44d5b0d015ca71
Mejanur Rahman Mezan <mrmezan06@gmail.com>
2023-02-12 03:10:53 +0600

    Initial Server Setup

---
+++ server/index.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import multer from 'multer';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+/* EXTENSION FOR CONSOLE COLOR */
+import 'colors';
+
+/* CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url); // ES6 way to get __filename when use ES6 modules
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/assets');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = process.env.MONGO_URL;
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server running on port: ${PORT}`.inverse.green)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`.inverse.red));
